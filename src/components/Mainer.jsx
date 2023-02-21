507d31ca926636c1afbfbebf7ca19dc88de306ad
wojciechdrabczyk <wojciechdrabczyk@pm.me>
2023-02-21 18:38:57 +0100

    finished practice page in react

--- src/components/Mainer.jsx
+++ src/components/Mainer.jsx
@@ -1,3 +1,14 @@
 export const Mainer = () => {
-  return <h1>Mainer component</h1>;
+  return (
+    <main>
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,11 @@
 export const NavBar = () => {
   return (
-    <>
+    <nav>
       <a href="https://beta.reactjs.org/" target="_blank">
-        <img
-          src="../images/react-icon-small.png"
-          className="logo"
-          alt="logo of React"
-        />
+        <img src="../images/react-icon-small.png" alt="logo of React" />
       </a>
-      <h1>ReactFacts</h1>
-    </>
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
   );
 };
